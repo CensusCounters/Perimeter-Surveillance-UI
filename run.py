be772131aa14fbd30da3fa820fdeb91f9f689f22
root <root@Sankar>
2024-02-26 15:04:29 +0530

    kafka integration changes

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from finalfrsproject import app
+from finalfrsproject import app, socketio
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000, threaded=True, debug=True)
+    # app.run(host='0.0.0.0', port=4000, threaded=True, debug=True)
+    socketio.run(app, host='0.0.0.0', port=4000, debug=True)
