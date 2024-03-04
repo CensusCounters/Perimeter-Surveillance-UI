6849b9e9b6e86effe77b45693920a5c072ba0f72
root <root@Sankar>
2024-03-04 12:30:59 +0530

    Kafka consumer change

--- finalfrsproject/kafka_consumer.py
+++ finalfrsproject/kafka_consumer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer, errors
 import json
-from finalfrsproject import socketio, sqlCommands
+# from finalfrsproject import socketio, sqlCommands
 import time
 import pathlib
 from datetime import datetime
@@ -10,7 +10,11 @@ MAX_ALERTS_NUMBER = 10 # after that pause for SENDING_PAUSE_TIME
 MIN_ALERTS_THRESHOLD = 2 # Alerts needed within MAX_ALERTS_INTERVAL to activate sending
 MAX_ALERTS_INTERVAL = 10 # interval in seconds
 TIMESTAMP_FORMAT='%Y-%m-%dT%H:%M:%S.%fZ'
-TOPIC_NAME = 'fire'
+TOPIC_NAME = 'events'
+
+KAFKA_SERVERS='localhost:9092'
+
+print(f"AlertSender is loaded and connected to topic '{TOPIC_NAME}'")
 
 alerts_calculator = {}
 last_sent_timestamp = {}
@@ -19,23 +23,22 @@ count_sent_messages = {}
 def start_kafka_consumer():
     try:
         consumer = KafkaConsumer(
-            'bounding_box_data',
-            bootstrap_servers=['localhost:9092'],
+            TOPIC_NAME,
+            bootstrap_servers=KAFKA_SERVERS,
             value_deserializer=lambda x: json.loads(x.decode('utf-8')),
             auto_offset_reset='latest',
             enable_auto_commit=True,
             group_id="99999",
         )
         
-        for message in consumer:
-            if message:
-                # print(f"Received message: {message.value}")
-
+        for event in consumer:
+            if event:
                 print("#################### KAFKA BEGIN ####################")
-                event_data = message.value
-                camera_id = event_data['camera_id']
-                message_timestamp = datetime.strptime(event_data['time'], TIMESTAMP_FORMAT)
-                print(f"Message consumed from camera {camera_id} with timestamp {event_data['time']}")
+                print(f"Received event: {event.value}")
+                event_data = event.value
+                camera_id = event_data['sensor']['id']
+                message_timestamp = datetime.strptime(event_data['@timestamp'], TIMESTAMP_FORMAT)
+                print(f"Message consumed from camera {camera_id} with timestamp {event_data['@timestamp']}")
 
                 if camera_id in alerts_calculator.keys():
                     alerts_calculator[camera_id] = [x for x in alerts_calculator[camera_id] if (message_timestamp - x).total_seconds() <= MAX_ALERTS_INTERVAL]
@@ -43,12 +46,15 @@ def start_kafka_consumer():
                     alerts_calculator[camera_id] = [] 
                 alerts_calculator[camera_id].append(message_timestamp)
 
-                print("alerts_calculator ----- ", alerts_calculator)
-
                 if camera_id not in count_sent_messages.keys():
                     count_sent_messages[camera_id] = 0
-                
-                print("count_sent_messages ----- ", count_sent_messages)
+
+                file_extension = pathlib.Path(event_data['videoPath']).suffix
+                curr_time = round(time.time()*1000)
+                imgpath = event_data['videoPath']
+                filename = 'uploadedfile-sample-' + str(curr_time) + file_extension
+                # imgUrl = Utils.uploadimgToS3(imgpath, filename)
+                #print(imgUrl)
                 
                 if (len(alerts_calculator[camera_id]) >= MIN_ALERTS_THRESHOLD):
                     if count_sent_messages[camera_id] >= MAX_ALERTS_NUMBER:
@@ -60,19 +66,29 @@ def start_kafka_consumer():
                                 f'{count_sent_messages[camera_id]} messages already sent for this camera within last {SENDING_PAUSE_TIME} minutes. '
                                 f'(Last sent message timestamp={last_sent_timestamp[camera_id]})')
                     if count_sent_messages[camera_id] < MAX_ALERTS_NUMBER:
-                        print(f"Send alert to socket message: {event_data}")
+                        phone_numbers = str(event_data['analyticsModule']['id']).split(';')
+                        for number in phone_numbers:
+                            to = number
+                            msg = event_data['analyticsModule']['description'] + ' ' + event_data['sensor']['description'] + '. Please check image and press Yes to confirm fire. Press No for false alarms.'
+                            print(msg)
+                            # try:
+                            #     resp = Utils.sendWAmsg(imgUrl,msg,headers,to)
+                            #     resp.raise_for_status()
+                            # except requests.exceptions.HTTPError as err:
+                            #     print(err)
+                            #     sys.exit(1)
 
-                        try:
-                             alert_id = sqlCommands.insert_alert_detection(message.value)
-                        except Exception as e:
-                             print(f"Error inserting alert detection: {e}")
-                             alert_id = ''
-                        print(f"Received message - alert_id: {alert_id}")
-                        socketio.emit('new_message', {'data': message.value, 'alert_id': alert_id})
-                        
+                            # try:
+                            #     alert_id = sqlCommands.insert_alert_detection(event.value)
+                            # except Exception as e:
+                            #     print(f"Error inserting alert detection: {e}")
+                            #     alert_id = ''
+                            # print(f"Received message - alert_id: {alert_id}")
+                            # socketio.emit('new_message', {'data': event.value, 'alert_id': alert_id})
+
                         last_sent_timestamp[camera_id] = message_timestamp
                         count_sent_messages[camera_id]+=1
-                        print(f'Message#{count_sent_messages[camera_id]} for camera {camera_id} just sent to socket')
+                        print(f'Message#{count_sent_messages[camera_id]} for camera {camera_id} just sent (to all whatsapp numbers)')
                 else:
                     print(f'Not sending message for camera {camera_id} because it was less than {MIN_ALERTS_THRESHOLD} messages within last {MAX_ALERTS_INTERVAL} seconds')
 
